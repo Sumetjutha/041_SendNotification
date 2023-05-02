f4811312d59b71a4fd05df5ca1cbdc362c241ec2
Sumet Jutajan <sumet.jutha@gmail.com>
2023-05-02 23:26:58 +0700

    Update resolution_reminder.py

--- resolution_reminder.py
+++ resolution_reminder.py
@@ -8,9 +8,9 @@ API_KEY = os.getenv('API_KEY')
 
 file = "resolution.txt"
 
-with open(file, mode='r') as f:
-    # print(f.read())
-    text = f.read()
+with open(file, mode='rb') as f:
+    # อ่านไฟล์เป็น binary และแปลงเป็น Unicode
+    text = f.read().decode('utf-8')
     
 pb = Pushbullet(API_KEY)
 push = pb.push_note('Please remember', text)
